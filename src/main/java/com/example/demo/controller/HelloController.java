03353b96055973febcfc95f83c8b08fa3fd0ba37
autopp <autopp.inc@gmail.com>
2023-02-10 22:09:34 +0900

    feat: add upper

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,20 +1,33 @@
 package com.example.demo.controller;
 
-import lombok.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.List;
 
-@Value
-class HelloResponse {
-    private final String message;
+
+record GreetResponse(String message) {
+}
+
+record UpperRequest(List<String> words) {
+}
+
+record UpperResponse(List<String> words) {
 }
 
 @RestController
 public class HelloController {
     @GetMapping("/greet")
-    public Mono<HelloResponse> greet() {
-        return Mono.just(new HelloResponse("hello"));
+    public Mono<GreetResponse> greet() {
+        return Mono.just(new GreetResponse("hello"));
+    }
+
+    @PostMapping("/upper")
+    public Mono<UpperResponse> upper(@RequestBody UpperRequest param) {
+        return Flux.fromIterable(param.words()).map(String::toUpperCase).collectList().map(UpperResponse::new);
     }
 }
